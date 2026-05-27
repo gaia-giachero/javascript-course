53884dce95a69184c0191f627a09cc7c0e69388e
Gaia Giachero <gaia.giachero@edu-its.it>
2026-05-27 10:46:38 +0200

    U2.2-calculate-age: calculate age and write message in the console with variables

---
+++ 02-variables-types-and-operators/02-calculate-age/script.js
@@ -0,0 +1,15 @@
+/* 
+    ● Store your birth year in a variable.
+    ● Store a future year in a variable.
+    ● Calculate your 2 possible ages for that year based on the stored values.
+    ● For example, if you were born in 1988, then in 2026 you'll be either 37 or 38, depending on what month it is in 2026.
+    ● Output them to the console like so: "I will be either NN or NN in YYYY", substituting the values.
+*/
+
+let yearBorn = 2006;
+let yearToday = 2026;
+
+let age = yearToday - yearBorn;
+let age1 = (yearToday - yearBorn) - 1;
+
+console.log(`I will be either ${age} or ${age1} in ${yearToday}`);
