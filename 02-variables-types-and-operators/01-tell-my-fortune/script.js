0775cb4b387394c5a72d8844e04e8d321622cb93
Gaia Giachero <gaia.giachero@edu-its.it>
2026-05-27 10:35:15 +0200

    U2.1-tell-my-fortune: first message in console with variables

---
+++ 02-variables-types-and-operators/01-tell-my-fortune/script.js
@@ -0,0 +1,15 @@
+/* 
+    Store the following into variables: number of children, partner's name, geographic location, job title.
+    Output your fortune to the console like so: "You will be a X in Y, and married to Z with N kids."
+
+    Memorizza le seguenti informazioni in variabili: numero di figli, nome del partner, posizione geografica, titolo di lavoro.
+    Mostra il tuo destino nella console così:
+        "You will be a X in Y, and married to Z with N kids."
+*/
+
+let numChildren = 3;
+let namePartner = 'Riccardo';
+let geoLocation = 'Los Angeles';
+let jobTitle = 'Engineer';
+
+console.log(`You will be a ${jobTitle} in ${geoLocation}, and married to ${namePartner} to with ${numChildren} kids.`)
