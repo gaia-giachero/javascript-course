48a6bf505be0627f9db15e1b8b2a25a596b185c2
Gaia Giachero <gaia.giachero@edu-its.it>
2026-05-27 10:39:29 +0200

    added doc on the js file

--- 02-variables-types-and-operators/01-tell-my-fortune/script.js
+++ 02-variables-types-and-operators/01-tell-my-fortune/script.js
@@ -1,10 +1,6 @@
 /* 
     Store the following into variables: number of children, partner's name, geographic location, job title.
     Output your fortune to the console like so: "You will be a X in Y, and married to Z with N kids."
-
-    Memorizza le seguenti informazioni in variabili: numero di figli, nome del partner, posizione geografica, titolo di lavoro.
-    Mostra il tuo destino nella console così:
-        "You will be a X in Y, and married to Z with N kids."
 */
 
 let numChildren = 3;
